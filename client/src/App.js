8df9dbb35ebaf035ab8c1b1441fd8cfb35fda6b1
Srinjoy-Santra <srinjoy_s@yahoo.com>
2020-06-10 00:44:40 +0530

    remove the body margin

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,27 @@
 import React from 'react';
-import { Route , BrowserRouter, Switch } from 'react-router-dom';
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
 
 import Home from './views/pages/Home';
 import Learn from './views/pages/Learn';
 import Detail from './views/pages/Detail';
 
-function App(){
-    return(
+function App() {
+    return (
         <div className="App">
             <BrowserRouter>
                 <Switch>
-                    <Route exact path='/' component={Home}/>
-                    <Route exact path='/learn' component={Learn}/>
-                    <Route exact path='/learn/:word' component={Detail}/>
+                    <Route exact path='/' component={Home} />
+                    <Route exact path='/learn' component={Learn} />
+                    <Route exact path='/learn/:word' component={Detail} />
                     {/* <Route path='*' component={NotFound}/> */}
                 </Switch>
             </BrowserRouter>
+            <style jsx global>{`
+                body {
+                    margin: 0px;
+                    padding: 0px;
+                }`}
+            </style>
         </div>
     )
 }
