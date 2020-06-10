14caea6ab24ccda8d40614b0d0182ea099aeccf9
Srinjoy-Santra <srinjoy_s@yahoo.com>
2020-06-10 18:14:37 +0530

    remove pagination grids

--- client/src/views/components/learn/Pagination.jsx
+++ client/src/views/components/learn/Pagination.jsx
@@ -4,48 +4,55 @@ import { makeStyles } from '@material-ui/core/styles';
 import IconButton from '@material-ui/core/IconButton'
 import SkipNextIcon from '@material-ui/icons/SkipNext';
 import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
-import Grid from '@material-ui/core/Grid';
-
 
 import PAGE_CONSTANTS from '../../../utils/pagination';
+import { Paper, Typography } from '@material-ui/core';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
     text: {
-        padding: 2
+        padding: 8,
+        textAlign: "center"
     },
-    
-});
+    controls: {
+        display: 'flex',
+        alignItems: 'center',
+        paddingLeft: theme.spacing(1),
+        paddingBottom: theme.spacing(1),
+        maxWidth: 240,
+        justifyContent: 'center',
+        margin: 'auto',
+        paddingTop: 8
+    }
+}));
 
 export default function WordPagination(props) {
     const classes = useStyles();
     const { START, END, RANGE } = PAGE_CONSTANTS;
     
-    {/*"https://source.unsplash.com/pVmjvK44Dao/275X140"*/}
     return (
-        <Grid container spacing={1}>
-            <Grid item xs={4}></Grid>
-            <Grid  item xs={1} spacing={3}>
-                <IconButton
+        <Paper className={classes.controls} elevation={3} >
+            <IconButton
                     aria-label="previous page"
+                    color="secondary"
+                    edge="end"
+                    className={classes.nextButton}
                     onClick={()=>props.handleMovePage(false)}
                     disabled={props.page === START/RANGE}
                 >
                     <SkipPreviousIcon/>
-                </IconButton>
-            </Grid>
-            <Grid  item xs={1} spacing={3}>
-                <p className={classes.text}>{props.page + 1}/5</p>
-            </Grid>
-            <Grid  item xs={1} spacing={3}>
-                <IconButton
+            </IconButton>
+            <Typography className={classes.text} variant="subtitle1" color="secondary">
+                {props.page + 1}/5
+            </Typography>
+            <IconButton
                     aria-label="next page"
+                    color="secondary"
+                    className={classes.nextButton}
                     onClick={()=>props.handleMovePage(true)}
                     disabled={props.page === parseInt((END/RANGE))}   
-                >
-                    <SkipNextIcon/>
-                </IconButton>
-            </Grid>
-            <Grid item xs={4}></Grid>
-        </Grid>
+            >
+                <SkipNextIcon/>
+            </IconButton>
+        </Paper>
     );
 }
