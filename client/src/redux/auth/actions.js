529aa10ee9ce2400945b6aeef42aa0618a40ac32
Srinjoy-Santra <srinjoy_s@yahoo.com>
2020-06-17 17:34:30 +0530

    v 1.0.0

--- client/src/redux/auth/actions.js
+++ client/src/redux/auth/actions.js
@@ -20,11 +20,18 @@ const register = (dispatch, formValues ) => {
 
     axios
         .post('/api/users', body, config)
-        .then(res =>
+        .then(res => {
+            alertActions.setAlert(dispatch, {
+                status: res.status,
+                msg: { msg: `Welcome, ${res.data.user.name}` },
+                id: types.REGISTER_SUCCESS,
+                severity: 'success'
+            })
             dispatch({
                 type: types.REGISTER_SUCCESS,
                 payload: res.data
             })
+        }
         )
         .catch(err => {
 
@@ -51,14 +58,19 @@ const login = (dispatch, formValues ) => {
 
     axios
         .post('/api/auth', body, config)
-        .then(res =>
+        .then(res => {
+            alertActions.setAlert(dispatch, {
+                status: res.status,
+                msg: { msg: `Hi, ${res.data.user.name}` },
+                id: types.LOGIN_SUCCESS,
+                severity: 'success'
+            })
             dispatch({
                 type: types.LOGIN_SUCCESS,
                 payload: res.data
             })
-        )
+        })
         .catch(err => {
-
             alertActions.setAlert(dispatch, {
                 status: err.response.status,
                 msg: err.response.data,
@@ -71,10 +83,18 @@ const login = (dispatch, formValues ) => {
         });
 };
 
-const logout = (dispatch) => 
+const logout = (dispatch) => {
+    alertActions.setAlert(dispatch, {
+        status: 200,
+        msg: { msg: `Bye, we will miss you!` },
+        id: types.LOGOUT_SUCCESS,
+        severity: 'warning'
+    })
     dispatch({
         type: types.LOGOUT_SUCCESS
     })
+}
+    
 
 export default{
     setAuthModal,
